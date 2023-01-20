480a205002e9f1d5c6d76e6b553bd659b4c508e3
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-20 10:48:22 +0000

    commit tree a8ce742724118b6e4ca789d2279ec45bbeb8c24f

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,19 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+# Build a class EmailAddressParser that accepts a string of unformatted emails. 
+
+class EmailAddressParser
+
+    attr_reader :emails_str
+
+    def initialize emails_str
+        @emails_str = emails_str
+    end
+
+    def parse
+        arr = @emails_str.split(/\W\s|\s/)
+        arr.uniq
+    end
+end
+
+
+# The parse method on the class should separate them intounique email addresses. 
+# The delimiters to support are commas (',') or whitespace (' ').
